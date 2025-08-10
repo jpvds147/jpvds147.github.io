390bfe42c184f41b5e46e5e105ed4545c31cf3ff
jpvds147 <44553678+jpvds147@users.noreply.github.com>
2025-08-10 16:14:26 +0200

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,7 @@
+<script> const menuToggle = document.getElementById('menu-toggle');
+const mainMenu = document.getElementById('main-menu');
+
+menuToggle.addEventListener('click', () => {
+  mainMenu.classList.toggle('show');
+});
+</script> 
